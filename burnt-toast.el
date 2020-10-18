3d692d1ae3db4e9bcd2c581defadd0f32ee59d17
Samuel Cedarbaum <scedarbaum@gmail.com>
2020-10-18 01:10:26 -0700

    Cleanup private methods, add better string sanitization.

--- burnt-toast.el
+++ burnt-toast.el
@@ -22,46 +22,64 @@
   :type 'string
   :group 'burnt-toast)
 
-(defun burnt-toast/quote-ps-arg (arg)
-  "Surround ARG with double quotes when it is non-nil."
-  (when arg
-    (concat "\"" arg "\"")))
+;; From: https://github.com/mplscorwin/erc-burnt-toast/blob/master/erc-burnt-toast.el
+(defun burnt-toast--clean-powershell-input (string)
+  "Return a version of STRING sanitized for use as input to PowerShell.
+<> are replaced with [], new-line is removed, and single-quotes are doubled."
+  (when (stringp string)
+    (org-no-properties
+     (replace-regexp-in-string
+      "\s+$" ""
+      (replace-regexp-in-string
+       "<" "["
+       (replace-regexp-in-string
+        ">" "]"
+        (replace-regexp-in-string
+         "[\t\n\r]+" ""
+         (replace-regexp-in-string
+          "'" "''"
+          (replace-regexp-in-string
+           "\"" "\"\""
+           string)))))))))
 
-(defun burnt-toast/string-nil-to-empty (arg)
-  "Return ARG when a non-nil string or an empty string otherwise."
-  (if (and arg (stringp arg))
-      arg
+(defun burnt-toast--quote-and-sanitize-string (string)
+  "Surround STRING with double quotes when it is non-nil."
+  (when string
+    (concat "\"" (burnt-toast--clean-powershell-input string) "\"")))
+
+(defun burnt-toast--nil-string-to-empty (string)
+  "Return STRING when a non-nil string or an empty string otherwise."
+  (if (stringp string)
+      string
     ""))
 
-(defun burnt-toast/run-powershell-command (command-and-args)
+(defun burnt-toast--run-powershell-command (command-and-args)
   "Execute a PowerShell command COMMAND-AND-ARGS."
   (let* ((ps-base-command (list burnt-toast-powershell-command nil nil nil))
          (all-args (add-to-list 'ps-base-command command-and-args t)))
+    (message command-and-args)
     (apply 'call-process all-args)))
 
-(defun burnt-toast/new-ps-object (object args)
+(defun burnt-toast--new-ps-object (object args)
   "Create a new PowerShell OBJECT using ARGS."
-  (let* ((prefix-string (concat "$(New-" object))
+  (let* ((prefix-string (concat "$(New-" object " "))
          (non-nil-args (-filter (-lambda ((_ value)) value) args))
          (args-string-list (-map
-                            (-lambda ((arg value)) (concat "-" arg " " (burnt-toast/string-nil-to-empty value)))
+                            (-lambda ((arg value)) (concat "-" arg " " (burnt-toast--nil-string-to-empty value)))
                             non-nil-args))
-         (args-string (seq-reduce (lambda (s1 s2) (concat s1 " " s2)) args-string-list "")))
+         (args-string (-reduce (lambda (s1 s2) (concat s1 " " s2)) args-string-list)))
     (concat prefix-string args-string ")")))
 
-(defun burnt-toast/bt-header-object (id title)
-  "Create a new BTHeader with ID and TITLE."
-  (burnt-toast/new-ps-object
-   "BTHeader"
-   `(("Id"    ,id)
-     ("Title" ,(burnt-toast/quote-ps-arg title)))))
 
-(cl-defun burnt-toast/new-notification-core (&key text app-logo sound header silent snooze-and-dismiss)
+(cl-defun burnt-toast--new-notification-core (&key text app-logo sound header silent snooze-and-dismiss)
   "Create new notification with subset of arguments.
 This function should not be called directly."
-  (let* ((combined-text (when (and text (listp text)) (seq-reduce (lambda (s1 s2) (concat s1 ",\"" s2 "\"")) text "\"")))
-         (processed-text (or (and combined-text (substring combined-text 2)) (burnt-toast/quote-ps-arg text)))
-         (ps-command (burnt-toast/new-ps-object
+  (let* ((processed-text (if (listp text)
+                             (-reduce
+                              (lambda (s1 s2) (concat s1 "," s2))
+                              (-map 'burnt-toast--quote-and-sanitize-string text))
+                           (burnt-toast--quote-and-sanitize-string text)))
+         (ps-command (burnt-toast--new-ps-object
                       "BurntToastNotification"
                       `(("Text"             ,processed-text)
                         ("AppLogo"          ,app-logo)
@@ -69,11 +87,18 @@ This function should not be called directly."
                         ("Header"           ,header)
                         ("Silent"           ,silent)
                         ("SnoozeAndDismiss" ,snooze-and-dismiss)))))
-    (burnt-toast/run-powershell-command ps-command)))
+    (burnt-toast--run-powershell-command ps-command)))
+
+(defun burnt-toast/bt-header-object (id title)
+  "Create a new BTHeader with ID and TITLE."
+  (burnt-toast--new-ps-object
+   "BTHeader"
+   `(("Id"    ,id)
+     ("Title" ,(burnt-toast--quote-and-sanitize-string title)))))
 
 (cl-defun burnt-toast/new-notification-with-sound (&key text app-logo sound header)
   "Create new notification with TEXT, APP-LOGO, SOUND, and HEADER."
-  (burnt-toast/new-notification-core
+  (burnt-toast--new-notification-core
    :text text
    :app-logo app-logo
    :sound sound
@@ -81,7 +106,7 @@ This function should not be called directly."
 
 (cl-defun burnt-toast/new-notification-silent (&key text app-logo header)
   "Create new notification with TEXT, APP-LOGO, and HEADER."
-  (burnt-toast/new-notification-core
+  (burnt-toast--new-notification-core
    :text text
    :app-logo app-logo
    :silent t
@@ -89,7 +114,7 @@ This function should not be called directly."
 
 (cl-defun burnt-toast/new-notification-snooze-and-dismiss-with-sound (&key text app-logo header sound)
   "Create new snooze-and-dismiss notification with TEXT, APP-LOGO, HEADER, and SOUND."
-  (burnt-toast/new-notification-core
+  (burnt-toast--new-notification-core
    :text text
    :app-logo app-logo
    :sound sound
@@ -98,7 +123,7 @@ This function should not be called directly."
 
 (cl-defun burnt-toast/new-notification-snooze-and-dismiss-silent (&key text app-logo header)
   "Create new snooze-and-dismiss notification with TEXT, APP-LOGO, and HEADER."
-  (burnt-toast/new-notification-core
+  (burnt-toast--new-notification-core
    :text text
    :app-logo app-logo
    :silent t
