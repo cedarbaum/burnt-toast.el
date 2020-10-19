fe47b6ddc0e13ce336699737f230697e5459e698
Samuel Cedarbaum <scedarbaum@gmail.com>
2020-10-18 20:10:15 -0700

    Cleanup file headers, add dash package dependency.

--- burnt-toast-alert.el
+++ burnt-toast-alert.el
@@ -1,13 +1,21 @@
 ;;; burnt-toast-alert.el --- BurntToast integration with alert package -*- lexical-binding: t; coding: utf-8 -*-
 
 ;; Copyright (C) 2020 Sam Cedarbaum
-;;
+
 ;; Author: Sam Cedarbaum (scedarbaum@gmail.com)
-;; Keywords: alert notifications powershell
-;; Homepage: https://github.com/cedarbaum/burnt-toast.el
-;; Version: 0.1
-;; Package-Requires: ((emacs "25.1"))
-;; License: GPL3
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
--- burnt-toast.el
+++ burnt-toast.el
@@ -1,13 +1,25 @@
 ;;; burnt-toast.el --- Elisp integration with BurntToast -*- lexical-binding: t; coding: utf-8 -*-
 
 ;; Copyright (C) 2020 Sam Cedarbaum
-;;
+
 ;; Author: Sam Cedarbaum (scedarbaum@gmail.com)
 ;; Keywords: alert notifications powershell
 ;; Homepage: https://github.com/cedarbaum/burnt-toast.el
 ;; Version: 0.1
-;; Package-Requires: ((emacs "25.1"))
-;; License: GPL3
+;; Package-Requires: ((emacs "25.1") (dash "2.10"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
